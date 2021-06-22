d0cf4e2a0ecc1c40141faf10a2a465122b3d5ba1
Joe Dietrich <joe@joedietrich.net>
2021-06-21 21:56:51 -0400

    Created Sidebar and added first mobile route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./App.css";
 import useWindowSize from "./hooks/useWindowSize";
 import Login from './components/Login'
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 import useAuthUser from "./hooks/useAuthUser";
+import { Route, Redirect } from "react-router-dom";
 
 export default function App() {
   const page = useWindowSize();
@@ -13,7 +16,15 @@ export default function App() {
   }
 
   return (
-    <div className="app" style={{ ...page }}>App</div>
+    <div className="app" style={{ ...page }}>
+      <Redirect to={page.isMobile ? "/chats" : "/"} />
+      <div className="app__body">
+        <Sidebar user={user} page={page} />
+        <Route path="/room/:roomId">
+          <Chat user={user} page={page} />
+        </Route>
+      </div>
+    </div>
   )
 
 }
